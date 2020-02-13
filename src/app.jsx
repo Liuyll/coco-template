dc47eec11b72246bffc26c2e0c079df2f75ceb76
Liuyll <540374501@qq.com>
2020-02-14 02:36:35 +0800

    add js suppose

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+if(module.hot){
+    module.hot.accept('./')
+}
+
+const App = function(){
+    return (
+        <div>
+            Welcome to Easy-React
+        </div>
+    )
+}
+
+export default App
